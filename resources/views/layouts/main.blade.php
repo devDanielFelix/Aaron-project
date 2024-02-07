ca15943ec33fa754fd81f5a0e7244acbd6882b05
devDanielFelix <danielfelix72@hotmail.com>
2024-02-07 18:49:45 -0300

    ok

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,57 +14,57 @@
     <!--bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+    <!-- styles -->
     <link rel="stylesheet" href="styles/styles.css">
 
 </head>
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-ligth">
-            <div class="collapse navbar-collapse" id="navbar">
-                <a href="/" class="navbar-brand">
-                    <img src="img/psyduck.png" alt="Icon">
-                </a>
-                <ul class="navbar-nav">
-                    <li class="nav-iten">
-                        <a href="/produtos" class="nav-link">Produtos</a>
-                    </li>
-                    <li class="nav-iten">
-                        <a href="/contatos" class="nav-link">Contatos</a>
-                    </li>
-                    <li class="nav-iten">
-                        <a href="/" class="nav-link">Entrar</a>
-                    </li>
-                    <li class="nav-iten">
-                        <a href="/" class="nav-link">Cadastrar</a>
-                    </li>
-                </ul>
-                <div id="search-container">
-                    <form action="/" method="GET">
-                        <input type="text" id="search" name="search" autocomplete="off" class="form-control" placeholder="Pesquisa Psyduck Store">
-                    </form>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-ligth">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="img/psyduck.png" alt="Icon">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-iten">
+                            <a href="/produtos" class="nav-link">Produtos</a>
+                        </li>
+                        <li class="nav-iten">
+                            <a href="/contatos" class="nav-link">Contatos</a>
+                        </li>
+                        <li class="nav-iten">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-iten">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                    <div id="search-container">
+                        <form action="/" method="GET">
+                            <input type="text" id="search" name="search" autocomplete="off" class="form-control" placeholder="Pesquisa Psyduck Store">
+                        </form>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if (session('msg'))
+                        <p class="msg">{{ session('msg') }}</p>
+                    @endif
+                    @yield('content')
+                </div>
             </div>
-        </nav>
-    </header>
+        </main>
 
-    <main>
-        <div class="container-fluid">
-            <div class="row">
-                @if (session('msg'))
-                    <p class="msg">{{ session('msg') }}</p>
-                @endif
-                @yield('content')
-            </div>
-        </div>
-    </main>
+        @yield('contet')
+        <footer>
+            <p>Psyduck Store - Artigos Esportivos &copy 2024</p>
+        </footer>
 
-    @yield('contet')
-    <footer>
-        <p>Psyduck Store - Artigos Esportivos &copy 2024</p>
-    </footer>
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-
-</body>
-
+    </body>
 </html>
